50f6931f067a2d02916c9a621a91c84e03cc8ab1
gawdeChinmay <gawdechinmay1234@gmail.com>
2025-01-03 00:18:32 +0530

    Added tab bar

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:my_flutter_app/pages/tab_bar1.dart';
+import 'package:my_flutter_app/pages/tab_bar2.dart';
+import 'package:my_flutter_app/pages/tab_bar3.dart';
 
 class NewsPage extends StatelessWidget {
   const NewsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: const Text("News"),
-      ),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+            ]),
+          ),
+          body: TabBarView(children: [TabBar1(), TabBar2(), TabBar3()]),
+        ));
   }
 }
