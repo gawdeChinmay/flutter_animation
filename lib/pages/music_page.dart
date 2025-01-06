443af033c5ad27d7854cc60e40ddf64308483718
gawdeChinmay <gawdechinmay1234@gmail.com>
2025-01-07 00:59:26 +0530

    added pageview navigation

--- lib/pages/music_page.dart
+++ lib/pages/music_page.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class MusicPage extends StatelessWidget {
+class MusicPage extends StatefulWidget {
   const MusicPage({super.key});
 
   @override
+  State<MusicPage> createState() => _MusicPageState();
+}
+
+class _MusicPageState extends State<MusicPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: const Text("Music"),
-      ),
+    return PageView(
+      children: [
+        Container(
+          color: Colors.yellow,
+          child: const Center(
+            child: Text(
+              "1",
+              style: TextStyle(fontSize: 100),
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.red,
+          child: const Center(
+            child: Text(
+              "2",
+              style: TextStyle(fontSize: 100),
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.green,
+          child: const Center(
+            child: Text(
+              "3",
+              style: TextStyle(fontSize: 100),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
